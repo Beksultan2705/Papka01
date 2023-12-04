657e3b790b666b235fe7b8b912220df3e3ac4074
Beksultan2705 <150647353+Beksultan2705@users.noreply.github.com>
2023-12-04 18:24:46 +0600

    martians.py

--- martians.py
+++ martians.py
@@ -1,27 +1,29 @@
-def find_cargo():
+import random
+
+def find_martian_cargo():
     total_weight = 713
     cargo_positions = [0, 0, 0]
-    
+
+    def tentacles_move_boxes(cargo_positions):
+        return [random.randint(1, 7) for _ in cargo_positions]
+
     while True:
-        print("Enter kilometer mark where the box may be buried (1-7):")
+        print("Enter the kilometer marks where the boxes may be buried (1-7):")
         for i in range(3):
-            kilometer_mark = int(input(f"Box {i + 1}: "))
-            
-            # Check if cargo is at the entered kilometer mark
+            kilometer_mark = int(input(f"Enter the kilometer mark for Box {i + 1}: "))
+
             if cargo_positions[i] == kilometer_mark:
                 print(f"Box {i + 1} found at {kilometer_mark} kilometers.")
             else:
                 print(f"Box {i + 1} not found at {kilometer_mark} kilometers. Tentacles are moving boxes.")
-                # Update box positions randomly
-                cargo_positions[i] = (cargo_positions[i] + 2) % 7 + 1
-        
-        # Check if the total weight is correct
+                cargo_positions = tentacles_move_boxes(cargo_positions)
+
         if sum(cargo_positions) == total_weight:
-            print("Congratulations! All boxes found, and total weight is correct.")
+            print("Congratulations! All boxes found, and the total weight is correct.")
             break
         else:
-            print("Total weight is incorrect. Try again.")
+            print("The total weight is incorrect. Try again.")
 
 if __name__ == "__main__":
     print("Martian Cargo Recovery Program")
-    find_cargo()
+    find_martian_cargo()
