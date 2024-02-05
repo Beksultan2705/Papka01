4f501397e5f5154147da941629faa0703f36dbe6
Beksultan2705 <150647353+Beksultan2705@users.noreply.github.com>
2024-02-05 17:56:23 +0600

    Create user.py

---
+++ user.py
@@ -0,0 +1,13 @@
+user_info['name'] = input('What is your name? ')
+
+user_info['age'] = input('What is your age? ')
+
+user_info['country'] = input('What is your country? ')
+
+user_info['known_for'] = input('What are you known for? ')
+
+print("\nUser Information:")
+print(f"Name: {user_info['name']}")
+print(f"Age: {user_info['age']}")
+print(f"Country: {user_info['country']}")
+print(f"Known For: {user_info['known_for']}")
