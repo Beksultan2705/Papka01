b42860baf1645419ea3a1678a7eef7c28e237bbf
Beksultan2705 <150647353+Beksultan2705@users.noreply.github.com>
2024-02-05 17:48:04 +0600

    Create laptop.py

---
+++ laptop.py
@@ -0,0 +1,9 @@
+laptop = {"brand": "dell", "model": "alienware", "year": 2020}
+
+print("laptop's Brand is: ", laptop["brand"])
+
+laptop["home"] = True
+print("Updated laptop:", laptop)
+
+laptop["year"] = 2019
+print("Updated year:", laptop["year"])
