c047283d2e981963d8e18ecac23aad244df851d2
Beksultan2705 <150647353+Beksultan2705@users.noreply.github.com>
2024-04-04 10:21:46 +0600

    Create HA4:Data_Tendecy.py

---
+++ HA4:Data_Tendecy.py
@@ -0,0 +1,1 @@
+//Explain the major central and variance of data (like Percentiles, Standard Deviation, Variance, Correlation, Correlation Matrix, Correlation vs Causality) with python code and submit the python practice file.
